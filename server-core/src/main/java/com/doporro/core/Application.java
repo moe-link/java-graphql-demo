0bc421b540e4517e61f29b59904fcb0260032661
moesama <moesama_zzt@163.com>
2023-08-18 18:21:49 +0800

    feat: init code

--- server-core/src/main/java/com/doporro/core/Application.java
+++ server-core/src/main/java/com/doporro/core/Application.java
@@ -3,7 +3,7 @@ package com.doporro.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.doporro.core")
 public class Application {
 
     public static void main(String[] args) {
