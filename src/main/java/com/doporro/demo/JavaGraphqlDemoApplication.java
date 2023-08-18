594c3d1461918c4b2ea2dd8fd9368709cf2ee6ca
moesama <moesama_zzt@163.com>
2023-08-18 18:10:47 +0800

    feat: init code

---
+++ src/main/java/com/doporro/demo/JavaGraphqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.doporro.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGraphqlDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGraphqlDemoApplication.class, args);
+    }
+
+}
